51eb01c0936b886216ca468b13353f208b45887f
emmanuel2612 <64177390+emmanuel2612@users.noreply.github.com>
2020-11-03 04:41:25 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ window.onload = () => {
     const header = document.getElementById('header');
     const logo = document.getElementById('logo');
     const headerLink = document.getElementsByClassName('header-link');
+    const mobileMenu = document.getElementById('mobile-menu');
 
     document.body.onscroll = () => {  
 
         if (document.documentElement.scrollTop > 10) {                    
             header.style.backgroundColor = "white";   
-            logo.style.color = "#191919";  
+            logo.style.color = "#191919"; 
+            mobileMenu.style.color = "#191919"; 
             
             for (let i = 0; i < headerLink.length; i++) {
                 headerLink[i].style.color = "#191919";
@@ -19,6 +21,7 @@ window.onload = () => {
         else {
             header.style.backgroundColor = "transparent";
             logo.style.color = "white"; 
+            mobileMenu.style.color = "white"; 
 
             for (let i = 0; i < headerLink.length; i++) {
                 headerLink[i].style.color = "white";
